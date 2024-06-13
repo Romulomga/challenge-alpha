0a0d49ccc8228acb03b1b58745b32686695e5169
Matheus Ferreira <Mathferreiranasc12@gmail.com>
2024-06-12 22:40:05 -0300

    feat/setup project

---
+++ Projects/HurbTravel/HurbTravel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HurbTravel
+//
+//  Created by Matheus Ferreira on 12/06/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
